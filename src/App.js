31cb28fab845d3b9c803fdc93654c99af4289013
Rafael Daminelli <rdaminelli@hotmail.com>
2023-01-12 13:17:17 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import './App.css';
+import Square from './Square';
+
+function App() {
+  const propVariable = 'This is a prop'
+  const [squares, setSquares] = useState(["","","","","","","","",""]);
+  const [player, setPlayer] = useState(true);
+  return (
+    <div className="App">
+      <Square
+        squares={squares}
+        setSquares={setSquares}
+        player={player}
+        setPlayer={setPlayer}
+      />
+    </div>
+  );
+}
+
+export default App;
